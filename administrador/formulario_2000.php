f604efb80486dd5e11f9bb0697d22ca5e7f678ec
jhonudenar <jhonudenar@gmail.com>
2016-06-05 15:38:18 -0500

    este cambio lo realizo el usuario jhonudenar

---
+++ administrador/formulario_2000.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * hange this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "Este es el formulario 2000";
+
